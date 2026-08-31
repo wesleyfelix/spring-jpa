9d8a1fdd63c0b63a5cc9135af6d03b44bf906e50
wesleyfelix <wesley.felixdelima96@hotmail.com>
2026-08-31 14:56:51 -0300

    iniciando a integração com gemini e chatgpt para traduzir a sinopse

--- src/main/java/br/com/alura/screenmatch/model/Serie.java
+++ src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -1,5 +1,8 @@
 package br.com.alura.screenmatch.model;
 
+import br.com.alura.screenmatch.service.ConsultaChatGPT;
+import br.com.alura.screenmatch.service.ConsultaGemini;
+
 import java.util.OptionalDouble;
 
 public class Serie {
@@ -18,7 +21,7 @@ public class Serie {
         this.genero = Categoria.fromString(dadosSerie.genero().split(",")[0].trim());
         this.atores = dadosSerie.atores();
         this.poster = dadosSerie.poster();
-        this.sinopse = dadosSerie.sinopse();
+        this.sinopse = ConsultaGemini.obterTraducao(dadosSerie.sinopse().trim());
     }
 
     public String getAtores() {
---
+++ src/main/java/br/com/alura/screenmatch/service/ConsultaGemini.java
@@ -0,0 +1,19 @@
+package br.com.alura.screenmatch.service;
+
+
+import com.google.genai.Client;
+import com.google.genai.types.GenerateContentResponse;
+
+public class ConsultaGemini {
+    public static String obterTraducao(String texto) {
+        Client client = Client.builder().apiKey("a").build();
+
+        GenerateContentResponse response =
+                client.models.generateContent(
+                        "gemini-3-flash-preview",
+                        "Traduza para língua portuguesa o seguinte trecho da forma mais direta, não pergunte mais nada, apenas faça: "+texto,
+                        null);
+
+        return response.text();
+    }
+}
